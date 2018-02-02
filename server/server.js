c1cbf75c10a7cb3cdd4ad2b7cd47696612aa0a2f
jsj618 <30374455+jsj618@users.noreply.github.com>
2018-02-02 08:50:31 -0900

    connect & disconnect events

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 
 
 const publicPath=path.join(__dirname,'/../public');
 
-//heroku
+
 const port=process.env.PORT || 3000;
 
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+    console.log('from server: new user connected..');
 
-app.listen(3000,()=>{
+    socket.on('disconnect',()=>{
+        console.log('from server: server disconnected');
+    });
+});
+
+
+server.listen(3000,()=>{
     console.log(`Server is running on ${port}`);
 });
