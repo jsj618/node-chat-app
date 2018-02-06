2bcb1335844a419340a1416b5a7c07b2503f19f2
jsj618 <30374455+jsj618@users.noreply.github.com>
2018-02-06 06:02:33 -0900

    pub to multiple tabs

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,11 @@ var socket=io();
 socket.on('connect', () => {
     console.log('from client: connected to browser');
 
-    //normally this data would be pulled from a form
+    /* //normally this data would be pulled from a form
     socket.emit('createMessage', {
         from: 'jeff',
         text: 'hi there'
-    });
+    }); */
 });
 
 socket.on('disconnect', () => { console.log('from client: disconnected to browser'); });
--- server/server.js
+++ server/server.js
@@ -19,14 +19,19 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('from server: new user connected..');
 
-    socket.emit('newMessage', {
+   /*  socket.emit('newMessage', {
         from: 'bill',
         text: 'hey',
         createAt: 123
-    }); 
+    }); */ 
 
     socket.on('createMessage',(message)=>{
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect',()=>{
